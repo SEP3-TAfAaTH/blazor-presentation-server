51b9fca4a7db64220307bb213ff67dab393f9824
Pupendo <michal.pupak@gmail.com>
2021-05-14 12:24:50 +0200

    Account Bugs

--- BlazorPresentationServer/Model/Account.cs
+++ BlazorPresentationServer/Model/Account.cs
@@ -5,6 +5,7 @@ namespace BlazorPresentationServer.Model
     public class Account
     {
         public long Id { get; set; }
+        [Required]
         public string Username { get; set; }
         [Required]
         [MinLength(6, ErrorMessage = "The password must be a minimum of 6 characters")]
